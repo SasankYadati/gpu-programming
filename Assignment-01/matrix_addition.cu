4b08b6479c50446f5718e7e9923dba2e6f65a456
SasankYadati <sai.sasank.yadati@gmail.com>
2023-12-07 17:55:56 +0530

    assignment 1 [wip]

--- Assignment-01/matrix_addition.cu
+++ Assignment-01/matrix_addition.cu
@@ -1,19 +1,7 @@
 #include <iostream>
 #include <cuda.h>
-
+#include <kernels.h>
 using namespace std;
-
-__global__ void per_row_kernel() {
-    //todo
-}
-
-__global__ void per_column_kernel() {
-    //todo
-}
-
-__global__ void per_element_kernel() {
-    //todo
-}
 
 int main() {
     int m,n;
