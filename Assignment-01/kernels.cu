920c1956cf7e3cb669195fbb0435f1d713ebcacb
SasankYadati <sai.sasank.yadati@gmail.com>
2023-12-15 10:54:19 +0530

    solve assignment 1

--- Assignment-01/kernels.cu
+++ Assignment-01/kernels.cu
@@ -3,15 +3,31 @@
 
 __global__ void per_row_kernel(int m,int n,int *A,int *B,int *C) {
     int id = blockIdx.x * blockDim.x + threadIdx.x;
-    for (int i=0; i<n; i++) {
-        C[id * m + i] = A[id * m + i] + B[id * m + i];
+    if (id < m) {
+        for (int i=0; i<n; i++) {
+            C[id * n + i] = A[id * n + i] + B[id * n + i];
+        }
     }
 }
 
 __global__ void per_column_kernel(int m,int n,int *A,int *B,int *C) {
-
+    int x = threadIdx.x;
+    int y = threadIdx.y;
+    int id = blockIdx.x * blockDim.x * blockDim.y + (x * blockDim.x + y);
+    if (id < n) {
+        for (int i=0; i<m; i++) {
+            C[i * n + id] = A[i * n + id] + B[i * n + id];
+        }
+    }
 }
 
 __global__ void per_element_kernel(int m,int n,int *A,int *B,int *C) {
-    
+    int x = blockIdx.x;
+    int y = blockIdx.y;
+    int blockId = x * gridDim.x + y;
+    int threadId = blockId * (blockDim.x * blockDim.y) + (threadIdx.x * blockDim.x + threadIdx.y);
+    int id = threadId;
+    if (id < m*n) {
+        C[id] = A[id] + B[id];
+    }
 }
--- Assignment-01/matrix_addition.cu
+++ Assignment-01/matrix_addition.cu
@@ -17,6 +17,22 @@ int main() {
     readMatrix(hmatrixA, m*n);
     readMatrix(hmatrixB, m*n);
     
+    printf("--------------------\n");
+    for (int i=0; i<m; i++) {
+        for (int j=0; j<n; j++) {
+            printf("%d ", hmatrixA[i * n + j]);
+        }
+        printf("\n");
+    }
+
+    printf("--------------------\n");
+    for (int i=0; i<m; i++) {
+        for (int j=0; j<n; j++) {
+            printf("%d ", hmatrixA[i * n + j]);
+        }
+        printf("\n");
+    }
+
     int *matrixA, *matrixB, *matrixC;
     cudaMalloc(&matrixA, m*n*sizeof(int));
     cudaMalloc(&matrixB, m*n*sizeof(int));
@@ -25,25 +41,51 @@ int main() {
     cudaMemcpy(matrixA, hmatrixA, m*n*sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(matrixB, hmatrixB, m*n*sizeof(int), cudaMemcpyHostToDevice);
 
-    int num_threads_per_block = m < BLOCKSIZE ? m : BLOCKSIZE;
     int num_blocks = ceil((float)m/BLOCKSIZE);
 
-    per_row_kernel<<<num_blocks,num_threads_per_block>>>(m, n, matrixA, matrixB, matrixC);
+    per_row_kernel<<<num_blocks,BLOCKSIZE>>>(m, n, matrixA, matrixB, matrixC);
 
     cudaMemcpy(hmatrixC, matrixC, m*n*sizeof(int), cudaMemcpyDeviceToHost);
 
+    printf("--------------------\n");
     for (int i=0; i<m; i++) {
         for (int j=0; j<n; j++) {
-            printf("%d ", hmatrixC[i * m + j]);
+            printf("%d ", hmatrixC[i * n + j]);
         }
         printf("\n");
     }
+
 
     num_blocks = ceil((float)n/(BLOCKSIZE*BLOCKSIZE));
     dim3 blockDims(BLOCKSIZE, BLOCKSIZE);
 
     per_column_kernel<<<num_blocks, blockDims>>>(m, n, matrixA, matrixB, matrixC);
     
+    cudaMemcpy(hmatrixC, matrixC, m*n*sizeof(int), cudaMemcpyDeviceToHost);
+
+    printf("--------------------\n");
+    for (int i=0; i<m; i++) {
+        for (int j=0; j<n; j++) {
+            printf("%d ", hmatrixC[i * n + j]);
+        }
+        printf("\n");
+    }
+
+    num_blocks = ceil((float)(m*n)/(BLOCKSIZE*BLOCKSIZE));
+    dim3 gridDims(num_blocks/2, num_blocks-(num_blocks/2)+1);
+
+    per_element_kernel<<<gridDims, blockDims>>>(m, n, matrixA, matrixB, matrixC);
+
+    cudaMemcpy(hmatrixC, matrixC, m*n*sizeof(int), cudaMemcpyDeviceToHost);
+
+    printf("--------------------\n");
+    for (int i=0; i<m; i++) {
+        for (int j=0; j<n; j++) {
+            printf("%d ", hmatrixC[i * n + j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
 
